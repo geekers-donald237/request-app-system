014cded2d0b0f6361de882e36f661d722605bff3
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2023-12-11 10:50:53 +0100

    Update AuthenticatedSessionController.php

--- Api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ Api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -13,23 +13,25 @@ use Illuminate\Support\Facades\Auth;
 class AuthenticatedSessionController extends Controller
 {
     /**
-     * Handle an incoming authentication request.
+     * Handle an i ncoming authentication request.
      */
-    public function store(LoginRequest $request): JsonResponse
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response([
-          'token' => $request->session()->token()
-        ])->json();
+//        return response([
+//          'token' => $request->session()->token()
+//        ])->json();
+        return response()->json(['message' => 'connexion réussie']);
+
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
@@ -37,6 +39,9 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return response()->noContent();
+//        return response()->noContent();
+        return response()->json(['message' => 'Déconnexion réussie']);
+
     }
 }
+
