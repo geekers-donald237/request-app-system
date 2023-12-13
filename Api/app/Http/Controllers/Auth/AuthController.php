276aef62c629bc6970fab3732d56830c4bef47e1
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-12-13 20:15:06 +0100

    [#setup] redo setup without laravel/breeze package

---
+++ Api/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+
+    }
+
+
+}
