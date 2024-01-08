28e5d7d9662bcd2191f4ec96a6a2af43db3f1317
Mckenydany20 <danymckeny@gmail.com>
2024-01-08 23:44:48 +0100

    etat

--- Api/app/Enums/RequestStateEnum.php
+++ Api/app/Enums/RequestStateEnum.php
@@ -4,23 +4,23 @@ namespace App\Enums;
 
 enum RequestStateEnum: string
 {
-    case ATTENTE_DE_SOUMISSION = 'en_attente_de_soumission'; //  l'état initial d'une demande avant que l'étudiant ne la soumette.
+    case ATTENTE_DE_SOUMISSION = 'attente_soumission'; //  l'état initial d'une demande avant que l'étudiant ne la soumette.
 
-    case ATTENTE_DE_VALIDATION = 'en_attente_de_validation'; // la demande a été soumise mais doit encore être validée par le système.
+    case ATTENTE_DE_VALIDATION = 'attente_validation'; // la demande a été soumise mais doit encore être validée par le système.
 
-    case ATTENTE_DE_TRAITEMENT = 'en_attente_de_traitement'; // la demande a été validée et est en attente de traitement par l'équipe responsable.
+    case ATTENTE_DE_TRAITEMENT = 'attente_traitement'; // la demande a été validée et est en attente de traitement par l'équipe responsable.
 
-    case EN_COURS_DE_TRAITEMENT = 'en_cours_de_traitement'; // la demande a été validée et est en attente de traitement par l'équipe responsable.
+    case EN_COURS_DE_TRAITEMENT = 'en_cours'; // la demande a été validée et est en attente de traitement par l'équipe responsable.
 
-    case ATTENTE_DE_DECISION = 'en_attente_de_decision'; // la demande a été traitée et est en attente d'une décision finale.
+    case ATTENTE_DE_DECISION = 'attente_decision'; // la demande a été traitée et est en attente d'une décision finale.
 
-    case ACCEPTEE = 'acceptee'; //  la demande a été approuvée.
+    case ACCEPTEE = 'Accepter'; //  la demande a été approuvée.
 
-    case REFUSEE = 'refusee'; // la demande a été refusée.
+    case REFUSEE = 'Refuser'; // la demande a été refusée.
 
-    case TERMINEE = 'terminee'; // la demande a été traitée et a abouti à une décision finale.
+    case TERMINEE = 'Terminer'; // la demande a été traitée et a abouti à une décision finale.
 
-    case EN_ATTENTE_DE_REPONSE_DE_L_ETUDIANT = 'en_attente_de_reponse_etudiant'; // la demande a été approuvée sous réserve de l'acceptation de l'étudiant, qui doit encore fournir une réponse.
+    case EN_ATTENTE_DE_REPONSE_DE_L_ETUDIANT = 'reponse_etudiant'; // la demande a été approuvée sous réserve de l'acceptation de l'étudiant, qui doit encore fournir une réponse.
 
 
     public static function values(): array
--- Front-CoreUI/src/app/features/constant/constant.ts
+++ Front-CoreUI/src/app/features/constant/constant.ts
@@ -6,13 +6,13 @@ export class UserRoleConstants {
 }
 
 export class RequestStateConstants {
-  static readonly ATTENTE_DE_SOUMISSION = 'en_attente_de_soumission';
-  static readonly ATTENTE_DE_VALIDATION = 'en_attente_de_validation';
-  static readonly ATTENTE_DE_TRAITEMENT = 'en_attente_de_traitement';
-  static readonly EN_COURS_DE_TRAITEMENT = 'en_cours_de_traitement';
-  static readonly ATTENTE_DE_DECISION = 'en_attente_de_decision';
-  static readonly ACCEPTEE = 'acceptee';
-  static readonly REFUSEE = 'refusee';
-  static readonly TERMINEE = 'terminee';
-  static readonly EN_ATTENTE_DE_REPONSE_DE_L_ETUDIANT = 'en_attente_de_reponse_etudiant';
+  static readonly ATTENTE_DE_SOUMISSION = 'attente_soumission';
+  static readonly ATTENTE_DE_VALIDATION = 'attente_validation';
+  static readonly ATTENTE_DE_TRAITEMENT = 'attente_traitement';
+  static readonly EN_COURS_DE_TRAITEMENT = 'en_cours';
+  static readonly ATTENTE_DE_DECISION = 'attente_decision';
+  static readonly ACCEPTEE = 'Accepter';
+  static readonly REFUSEE = 'Refuser';
+  static readonly TERMINEE = 'Terminer';
+  static readonly EN_ATTENTE_DE_REPONSE_DE_L_ETUDIANT = 'reponse_etudiant';
 }
