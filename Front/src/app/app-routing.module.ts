bbe475e9bb0085a369b82e1fb1969c6c2b5a9924
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2024-04-05 09:43:03 +0100

    first dir deletion

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -31,26 +31,6 @@ const routes: Routes = [
         loadChildren: () =>
           import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule)
       },
-      {
-        path: 'theme',
-        loadChildren: () =>
-          import('./views/theme/theme.module').then((m) => m.ThemeModule)
-      },
-      {
-        path: 'base',
-        loadChildren: () =>
-          import('./views/base/base.module').then((m) => m.BaseModule)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () =>
-          import('./views/buttons/buttons.module').then((m) => m.ButtonsModule)
-      },
-      {
-        path: 'forms',
-        loadChildren: () =>
-          import('./views/forms/forms.module').then((m) => m.CoreUIFormsModule)
-      },
 
       {
         path: 'icons',
