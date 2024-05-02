e6506914ca060d6cd78147f6cea46ae72f5fa3b5
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2024-05-02 04:16:51 +0100

    added icons to dashboard item and updates redireckt link for attachement

--- Api/database/seeders/UserSeeder.php
+++ Api/database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Enums\EmailEnum;
 use App\Enums\RuleEnum;
-use App\Events\SendMailEvent;
 use App\Helpers\HelpersFunction;
 use App\Models\Rule;
 use App\Models\Secretary;
@@ -12,13 +11,10 @@ use App\Models\Staff;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Nette\Utils\Helpers;
 
 class UserSeeder extends Seeder
 {
     public array $userData;
-    public array $staffData;
-    public array $secretaryData;
 
 
     /**
@@ -38,12 +34,12 @@ class UserSeeder extends Seeder
      */
     public function createdStudentUsers(): void
     {
-        User::factory()->create(['name' => 'Dany mckeny', 'email' => 'danymckeny@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::STUDENT->value)->first()->id);
+        User::factory()->create(['name' => 'Compte test eleve', 'email' => 'student@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::STUDENT->value)->first()->id);
 
-        Student::factory()->create(['user_id' => (User::whereEmail('danymckeny@gmail.com')->first()->id),
+        Student::factory()->create(['user_id' => (User::whereEmail('student@gmail.com')->first()->id),
             'matricule' => '21Q2915'
-            ,'department_id' => '4'
-            ,'level_id' => 3,
+            , 'department_id' => '4'
+            , 'level_id' => 3,
 
         ]);
 
@@ -61,16 +57,9 @@ class UserSeeder extends Seeder
      */
     public function createSecretaryUser(): void
     {
-        User::factory()->create(['name' => 'Jane doe', 'email' => 'janedoe@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::SECRETARY->value)->first()->id);
+        User::factory()->create(['name' => 'Compte test secretaire', 'email' => 'secretary@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::SECRETARY->value)->first()->id);
+        Secretary::factory()->create(['user_id' => (User::whereEmail('secretary@gmail.com')->first()->id), 'job_title' => 'secretaire dpt info', 'address' => 'Extension 2', 'phone_number' => '+237 699854525', 'department_id' => '4']);
 
-        Secretary::factory()->create(['user_id' => (User::whereEmail('janedoe@gmail.com')->first()->id), 'job_title' => 'secretaire dpt info', 'address' => 'Extension 2', 'phone_number' => '+237 699854525', 'department_id' => '4']);
-
-        $this->secretaryData = [
-            'name' => "Jane doe",
-            'email' => 'janedoe@gmail.com',
-            'password' => '123456789',
-        ];
-        HelpersFunction::sendEmail($this->secretaryData, EmailEnum::STATUT3->value);
     }
 
     /**
@@ -78,17 +67,11 @@ class UserSeeder extends Seeder
      */
     public function createStaffUser(): void
     {
-        User::factory()->create(['name' => 'John Doe',
-            'email' => 'johndoe@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::STAFF->value)->first()->id);
+        User::factory()->create(['name' => 'Compte test Enseigant',
+            'email' => 'teacher@gmail.com', 'password' => '123456789'])->rules()->attach(Rule::whereName(RuleEnum::STAFF->value)->first()->id);
 
-        Staff::factory()->create(['user_id' => (User::whereEmail('johndoe@gmail.com')->first()->id), 'job_title' => 'Enseignant ICT317', 'address' => 'Bloc Pedagogique Porte S107', 'phone_number' => '+237 699854525']);
+        Staff::factory()->create(['user_id' => (User::whereEmail('teacher@gmail.com')->first()->id), 'job_title' => 'Enseignant ICT317', 'address' => 'Bloc Pedagogique Porte S107', 'phone_number' => '+237 699854525']);
 
-        $this->staffData = [
-            'name' => 'John Doe',
-            'email' => 'johndoe@gmail.com',
-            'password' => '123456789',
-        ];
-        HelpersFunction::sendEmail($this->staffData, EmailEnum::STATUT3->value);
     }
 
     /**
